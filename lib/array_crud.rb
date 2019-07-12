c62c96a47b65201273744683c2cf5fc0f710485c
peaceful-cyborg-2337 <cmajewski@wesleyan.edu>
2019-07-12 20:09:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+puppies=["golden","poodle","beagle","bichon"]
+puppies<< "labrador"
 end
 
 def add_element_to_start_of_array(array, element)
