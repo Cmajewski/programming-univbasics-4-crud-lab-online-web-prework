9772551f334dd9cc9ffab5f2387ecc71fbb0572a
peaceful-cyborg-2337 <cmajewski@wesleyan.edu>
2019-07-12 20:16:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,9 +9,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   
-puppies= ["golden","poodle","beagle","bichon"]
-puppies<< "labrador"
-
+["golden","poodle","beagle","bichon"]
+add_element_to_end_of_array<< "dalmation"
 end
 
 def add_element_to_start_of_array(array, element)
